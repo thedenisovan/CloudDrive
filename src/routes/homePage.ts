83aa821974704f926d978074c36b5d90055fd1ef
Dainis Dilevka <thedenisovan@proton.me>
2025-12-10 10:33:47 +0000

    chore: removed shared access function

--- src/routes/homePage.ts
+++ src/routes/homePage.ts
@@ -1,16 +1,15 @@
 import { Router } from 'express';
-import sharedAccess from '../controllers/sharedAccess';
 import signinValidator from '../validators/signin.validator';
 import validatorMiddleware from '../validators/signin.validatorResult';
 import passport from 'passport';
 
 const homePage = Router();
 
-homePage.get('/', (req, res) => res.render('index', { sharedAccess }));
+homePage.get('/', (req, res) => res.render('index'));
 homePage.post(
   '/',
-  //   signinValidator,
-  //   validatorMiddleware,
+  signinValidator,
+  validatorMiddleware,
   passport.authenticate('local', {
     successRedirect: '/storage',
     failureRedirect: '/',
