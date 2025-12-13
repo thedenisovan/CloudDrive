3e5ed77c0bb615c75e0b40fc2a83235ec710608a
Dainis Dilevka <thedenisovan@proton.me>
2025-12-13 14:23:19 +0000

    feat: ui to indicate current folder

--- src/routes/storagePage.ts
+++ src/routes/storagePage.ts
@@ -3,7 +3,24 @@ import getFolders from '../middleware/getFolders.js';
 
 const storagePage = Router();
 
-storagePage.get('/', getFolders, async (req, res) => {
-  res.render('storage', { user: req.user, folders: req.folders });
+storagePage.get(`/:folders`, getFolders, (req, res) => {
+  console.log('hi');
+  res.render('storage', {
+    user: req.user,
+    folders: req.folders,
+  });
 });
+storagePage.get('/', getFolders, async (req, res) => {
+  res.render('storage', {
+    user: req.user,
+    folders: req.folders,
+    selectedFolder: req.query.folders,
+  });
+});
+
+storagePage.post('/', async (req, res) => {
+  res.redirect('storage');
+  console.log(req.query.folders);
+});
+
 export default storagePage;
