d2b38009902a9384f1a4ef8c72f189266252c436
Dainis Dilevka <thedenisovan@proton.me>
2025-12-17 14:40:45 +0000

    add: supabase script

--- src/routes/uploadPage.ts
+++ src/routes/uploadPage.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
 import getFolders from '../middleware/getFolders.js';
-import upload from '../middleware/multerConfig.js';
+// import upload from '../middleware/multerConfig.js';
 
 const uploadPage = Router();
 
 uploadPage.get('', getFolders, (req, res) => {
-  res.render('uploadFileForm', { folders: req.folders });
+  res.render('uploadFileForm', {
+    folders: req.folders,
+  });
 });
 
-uploadPage.post('', upload.single('file'), (req, res) => {
-  res.redirect('storage');
-});
+uploadPage.post(
+  '',
+  /*upload.single('file'),*/ (req, res) => {
+    res.redirect('storage');
+  }
+);
 
 export default uploadPage;
