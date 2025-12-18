0513127d68ee0645786325cfb7297ab60f4b24fa
Dainis Dilevka <thedenisovan@proton.me>
2025-12-18 13:54:56 +0000

    add: style to file card

--- src/routes/storagePage.ts
+++ src/routes/storagePage.ts
@@ -8,11 +8,9 @@ import getFiles from '../middleware/getFiles.js';
 const storagePage = Router();
 
 storagePage.get('/', getFolders, getFiles, async (req, res) => {
-  console.log(req.dbFile);
   res.render('storage', {
     user: req.user,
     folders: req.folders,
-    // sif new folder is created or active one deleted set curr folder to last in folders list
     selectedFolder:
       req.query.folders || req.folders[req.folders.length - 1].name,
     files: req.dbFile,
