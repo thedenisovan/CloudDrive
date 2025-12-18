2d7281d416ae557ae8c4cdc92254f4aaf31585d3
Dainis Dilevka <thedenisovan@proton.me>
2025-12-18 15:18:39 +0000

    add: storage space indicator

--- src/routes/storagePage.ts
+++ src/routes/storagePage.ts
@@ -4,12 +4,16 @@ import cdFolder from '../middleware/cdFolder.js';
 import folderValidator from '../validators/folder.validator.js';
 import folderValidMw from '../middleware/folder.validResults.js';
 import getFiles from '../middleware/getFiles.js';
+import { prisma } from '../lib/prisma.js';
 
 const storagePage = Router();
 
 storagePage.get('/', getFolders, getFiles, async (req, res) => {
   res.render('storage', {
     user: req.user,
+    profile: await prisma.profile.findUnique({
+      where: { userId: req.user!.id },
+    }),
     folders: req.folders,
     selectedFolder:
       req.query.folders || req.folders[req.folders.length - 1].name,
